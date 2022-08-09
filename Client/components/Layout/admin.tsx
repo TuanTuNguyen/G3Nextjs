790aff4a753ce6b238d5643371d748e42743c0ed
TuanTuNguyen <tuntph14942@fpt.edu.vn>
2022-08-09 12:21:01 +0700

    update layout admin

--- Client/components/Layout/admin.tsx
+++ Client/components/Layout/admin.tsx
@@ -43,40 +43,43 @@ const AdminLayout = ({ children }: LayoutProps) => {
                     <div className="relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0">
                         <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
                             <div className="flex-1 px-3 bg-white divide-y space-y-1">
-                                <ul className="space-y-2 pb-2">
-
+                            <ul className="space-y-2 pb-2">
                                     <li>
                                         <Link href={'/admin/news'}>
-                                        <a href='' className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-4 group ">
+                                            <a href='' className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-4 group ">
 
-                                    {/* <li>
+                                            </a>
+                                        </Link>
+
+
+                                    </li>
+                                    <li>
                                         <a href='/admin' className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-4 group ">
-
                                             <svg className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
                                             </svg>
                                             <span className="ml-3 flex-1 whitespace-nowrap">Users</span>
                                         </a>
-
+                                    </li>
                                     <li>
-                                    <Link href={'/admin/products'}>
-                                        <a href='' className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-4 group ">
-                                            <svg className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
-                                            </svg>
-                                            <span className="ml-3 flex-1 whitespace-nowrap">Products</span>
-                                        </a>
+                                        <Link href={'/admin/products'}>
+                                            <a href='' className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-4 group ">
+                                                <svg className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
+                                                </svg>
+                                                <span className="ml-3 flex-1 whitespace-nowrap">Products</span>
+                                            </a>
                                         </Link>
                                     </li>
                                     <li>
                                         <a href='/admin/categories' className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-4 group ">
                                             <svg className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
+<path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
                                             </svg>
                                             <span className="ml-3 flex-1 whitespace-nowrap">Categories</span>
                                         </a>
                                     </li>
-                                    {/* <li>
+                                    <li>
                                         <a className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-4 group ">
                                             <svg className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
@@ -91,7 +94,9 @@ const AdminLayout = ({ children }: LayoutProps) => {
                                             </svg>
                                             <span className="ml-3 flex-1 whitespace-nowrap">Sign Up</span>
                                         </a>
-                                    </li> */}
+                                    </li>
+
+
                                 </ul>
                             </div>
                         </div>
